e7a94576425e5dbbfdee6d18637de37abcd3c737
Dmitry <kirillovdmitrii_at@mail.ru>
2022-11-13 04:43:04 +0500

    realised all functional

---
+++ src/main/java/pro/sky/telegrambot/model/Task.java
@@ -0,0 +1,83 @@
+package pro.sky.telegrambot.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+@Entity
+public class Task {
+
+    @Id
+    @GeneratedValue
+    Integer id;
+    Long chatId;
+    String text;
+    LocalDateTime dataTime;
+
+    public Task(Long chatId, String text, LocalDateTime dataTime) {
+        this.chatId = chatId;
+        this.text = text;
+        this.dataTime = dataTime;
+    }
+
+    public Task() {
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Task task = (Task) o;
+        return Objects.equals(id, task.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Task{" +
+                "id=" + id +
+                ", chatId='" + chatId + '\'' +
+                ", text='" + text + '\'' +
+                ", dataTime=" + dataTime +
+                '}';
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Long getChatId() {
+        return chatId;
+    }
+
+    public void setChatId(Long chatId) {
+        this.chatId = chatId;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public LocalDateTime getDataTime() {
+        return dataTime;
+    }
+
+    public void setDataTime(LocalDateTime dataTime) {
+        this.dataTime = dataTime;
+    }
+}
